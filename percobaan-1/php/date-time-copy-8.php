291371de7e11ffee865b78a29139ab81bdb5ee5c
DannEruma <dannversi2@gmail.com>
2026-09-17 13:03:23 +0700

    penambahan file praktikum modul  0.1, 0.2, 1

---
+++ percobaan-1/php/date-time-copy-8.php
@@ -0,0 +1,8 @@
+<?php
+echo "Today is " . date("Y/m.d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is ". date("l") . "<br>";
+echo "© 2010-";
+echo date("Y") ;
+?>
---
+++ percobaan-1/php/sintak-dasar-1.php
@@ -0,0 +1,5 @@
+<h1>My frist PHP page</h1>
+
+<?php
+    echo "Hello World!";
+?>
